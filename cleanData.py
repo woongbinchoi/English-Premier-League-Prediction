ed3be9e04cb9f0dc5fcd8839b896e16266cdcba4
woongbinchoi <woongbinchoi@gmail.com>
2018-09-22 01:57:00 -0400

    cleaned some of the data. Cleaned csv files are located in data/cleaned. script used is cleandata.py .

---
+++ cleanData.py
@@ -0,0 +1,25 @@
+
+import ntpath
+import os
+import pandas as pd
+
+def clean(path, columns):
+    df = pd.read_csv(path, error_bad_lines=False)
+    df = df[columns]
+    
+    fileName = ntpath.basename(path)
+    head, _ = ntpath.split(path)
+    newDir = ntpath.join(head, 'cleaned')
+    if not os.path.exists(newDir):
+        os.makedirs(newDir)
+    
+    newfilePath = ntpath.join(newDir, fileName)
+    df.to_csv(newfilePath)
+
+if __name__ == "__main__":
+    columns = ['Date', 'HomeTeam', 'AwayTeam', 'FTHG', 'FTAG', 'FTR']
+    
+    for year in range(1993, 2019):
+        path = 'data/%s-%s.csv' % (year, year + 1)
+        clean(path, columns)
+
