e3b8052714af1b4ff8aced781b59590ba2e86091
woongbinchoi <woongbinchoi@gmail.com>
2018-09-29 03:04:42 -0400

    date column parsed

--- cleanData.py
+++ cleanData.py
@@ -1,4 +1,5 @@
 import ntpath
+from datetime import datetime as dt
 import os
 import pandas as pd
 from sofifaScraper import mergeOVAToCleanedAll
@@ -7,7 +8,16 @@ from currentStatus import addCurrentDetailsAll
 
 # clean the original raw data by storing only the columns that we need, and removing the rest.
 def clean(fromPath, toPath, columns):
-    print("Cleaning ", fromPath, "...")
+    def convertDate(date):
+        if date == '':
+            return None
+        else:
+            _, file = ntpath.split(toPath)
+            if file == '2002-2003.csv':
+                return dt.strptime(date, '%d/%m/%Y').date()
+            else:
+                return dt.strptime(date, '%d/%m/%y').date()
+
     df = pd.read_csv(fromPath, error_bad_lines=False)
     df = df[columns]
     df = df[pd.notnull(df['Date'])]
@@ -17,6 +27,7 @@ def clean(fromPath, toPath, columns):
 
     df['FTHG'] = df['FTHG'].astype(int)
     df['FTAG'] = df['FTAG'].astype(int)
+    df['Date'] = df['Date'].apply(convertDate)
     
     head, _ = ntpath.split(toPath)
     if not os.path.exists(head):
@@ -28,7 +39,9 @@ def cleanAll(fromFolder, toFolder, columns):
         csvFile = '%s-%s.csv' % (year, year + 1)
         frompath = ntpath.join(fromFolder, csvFile)
         topath = ntpath.join(toFolder, csvFile)
+        print("Cleaning ", frompath, "...")
         clean(frompath, topath, columns)
+
 
 if __name__ == "__main__":
     RAW_DATA_FILE_PATH = 'data/raw'
